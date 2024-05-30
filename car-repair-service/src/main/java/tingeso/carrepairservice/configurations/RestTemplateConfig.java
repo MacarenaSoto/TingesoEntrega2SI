7a56082dd43ae2cb61cb7d7ade62789a4012b89b
Macarena Soto <macarena.soto.h@usach.cl>
2024-05-29 21:47:10 -0400

    mas cambios

--- car-repair-service/src/main/java/tingeso/carrepairservice/configurations/RestTemplateConfig.java
+++ car-repair-service/src/main/java/tingeso/carrepairservice/configurations/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package tingeso.carrepairservice.config;
+package tingeso.carrepairservice.configurations;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
